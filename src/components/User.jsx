7879a12685c8047ef85c881f7c8e7a07e0dc3ec9
ipmcknz33 <i.p.mcknz33@gmail.com>
2026-01-07 08:55:10 -1000

    small error

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,9 @@
-function User({ id, name, username, email, username }) {
+function User({ id, name, username, email, }) {
  
   return (
       <div style={{ border: `2px solid black` }}>
         <div>{id}</div>
-        <div>{name.toUpperCase()}</div>
+        <div>{name?.toUpperCase()}</div>
         <div>{username}</div>
         <div>{email}</div>
       </div>
